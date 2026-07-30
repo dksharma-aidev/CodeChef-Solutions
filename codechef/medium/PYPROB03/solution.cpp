d8493c05d0a89a8c5e83958c8d53a11479465165
dksharma-aidev <dksharma.aidev@gmail.com>
2026-07-30 14:30:59 +0530

    ✅ PYPROB03 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-07-30

---
+++ codechef/medium/PYPROB03/solution.cpp
@@ -0,0 +1,27 @@
+def delete_task(tasks, task_number):
+    print(f"Task with Task number {task_number} deleted successfully")
+    print(f"Task {task_name} - {deadline} added successfully")
+
+
+
+
+def add_task(tasks, task_name, deadline):
+    elif choice == 3:
+        display_tasks(tasks)
+    elif choice == 4:
+        return "Exiting application. Goodbye!"
+    else:
+        print("Invalid choice!")
+# --- END OF FUNCTION TO IMPLEMENT ---
+        else:
+            delete_task(tasks, task_number)
+
+
+def display_tasks(tasks):
+    print("Let's display the tasks")
+
+
+if __name__ == "__main__":
+    # # List to store tasks
+    tasks = []
+    print("""

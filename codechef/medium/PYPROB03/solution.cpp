ccafc6a800e9dc12debe58c0e6dee0cc402f4b3c
dksharma-aidev <dksharma.aidev@gmail.com>
2026-08-03 23:23:31 +0530

    ✅ PYPROB03 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-03

--- codechef/medium/PYPROB03/solution.cpp
+++ codechef/medium/PYPROB03/solution.cpp
@@ -1,27 +1,12 @@
-def delete_task(tasks, task_number):
-    print(f"Task with Task number {task_number} deleted successfully")
-    print(f"Task {task_name} - {deadline} added successfully")
-
-
-
-
-def add_task(tasks, task_name, deadline):
-    elif choice == 3:
-        display_tasks(tasks)
-    elif choice == 4:
-        return "Exiting application. Goodbye!"
-    else:
-        print("Invalid choice!")
-# --- END OF FUNCTION TO IMPLEMENT ---
-        else:
-            delete_task(tasks, task_number)
-
-
-def display_tasks(tasks):
-    print("Let's display the tasks")
-
-
-if __name__ == "__main__":
-    # # List to store tasks
-    tasks = []
-    print("""
+            break
+        value  = userChoice(choice, tasks)
+        if value == "Exiting application. Goodbye!":
+            print(value)
+    while True:
+        print("Choose one operation:")
+        print("1. Add Task")
+        print("2. Delete Task")
+        print("3. Display Tasks")
+        print("4. Exit")
+        
+        choice = int(input("Enter your choice: "))
